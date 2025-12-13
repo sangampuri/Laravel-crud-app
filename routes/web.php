8646cee90587e412e073b1b3b357e69bdcf07d8c
Sangam Puri <sangampuree@gmail.com>
2025-12-13 19:28:55 +0545

    Add session handling routes and enhance customer model mutators

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\CustomerController;
+use Illuminate\Http\Request;
 
 Route::get('/welcome/{name?}', function ($name=null) {
     $data = compact('name');
@@ -46,3 +47,16 @@ Route::get('/customer/delete/{id}',[CustomerController::class,'delete'])->name('
 Route::get('/customer/edit/{id}',[CustomerController::class,'edit'])->name('customer.edit');
 Route::post('/customer/update/{id}',[CustomerController::class,'update'])->name('customer.update');
 Route::get('/customer/view',[CustomerController::class,'view']);
+
+
+###########################################################
+//!storing name in session
+Route::get('/set-session',function (Request $request){
+    $request->session()->put('name','sangam');
+    return "Session set";
+});
+
+//!Read session
+Route::get('/get-session', function (Request $request) {
+    return $request->session()->get('name');
+});
