03cb099b4edf276f25608be17e254f56fbcbba04
Sangam Puri <sangampuree@gmail.com>
2025-12-13 12:26:05 +0545

    Refactor CustomerController to enhance readability and maintainability; added delete, edit, and update methods.

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Customer;
 use Illuminate\Http\Request;
 
@@ -8,7 +9,10 @@ class CustomerController extends Controller
 {
     public function index()
     {
-        return view('customer');
+        $title = 'Customer Registration';
+        $url = url('/customer');
+        $data = compact('url', 'title');
+        return view('customer')->with($data);
     }
 
     public function store(Request $request)
@@ -18,21 +22,50 @@ class CustomerController extends Controller
         $customer->name = $request['name'];
         $customer->email = $request['email'];
         $customer->country = $request['country'];
-        $customer->state = $request['state'];        
-        $customer->gender = $request['gender'];        
-        $customer->address = $request['address']; 
-        $customer->dob = $request['dob'];      
+        $customer->state = $request['state'];
+        $customer->gender = $request['gender'];
+        $customer->address = $request['address'];
+        $customer->dob = $request['dob'];
         $customer->password = md5($request['password']);
         $customer->save();
-        return redirect('/customer/view'); 
-
+        return redirect('/customer/view');
     }
     public function view()
-    {   
-        $customers=Customer::all();
+    {
+        $customers = Customer::all();
         $data = compact('customers');
-        
-        
         return view('customer-view')->with($data);
+    }
+
+    public function delete($id)
+    {
+        $customer = Customer::find($id);
+        if (!is_null($customer)) {
+            $customer->delete();
+        }
+        return redirect('/customer/view');
+    }
+
+    public function edit($id)
+    {
+        $title = "Update Customer";
+        $url = url('/customer/update') . "/" . $id;
+        $customer = Customer::find($id);
+        $data = compact('customer', 'url', 'title');
+        return view('customer')->with($data);
+    }
+
+    public function update($id, Request $request)
+    {
+        $customer = Customer::find($id);
+        $customer->name = $request['name'];
+        $customer->email = $request['email'];
+        $customer->country = $request['country'];
+        $customer->state = $request['state'];
+        $customer->gender = $request['gender'];
+        $customer->address = $request['address'];
+        $customer->dob = $request['dob'];
+        $customer->save();
+        return redirect('/customer/view');
     }
 }
