7c4d0c0022eb388c74a41b1bb791aae8f493bcbf
Sangam Puri <sangampuree@gmail.com>
2025-12-13 19:29:52 +0545

    added mutator acututator

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,23 @@ class Customer extends Model
 {
     protected $table='customers';
     protected $primaryKey = 'id';
+
+    //!Transforms the name into uppercase before storing into database using "Mutator"
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name']= ucwords($value);
+    }
+
+    //*if the field name is like "first_name"
+    //!instead of " setNameAttribute($value)"
+    //! we have to use ""setUserNameAttribute()"
+
+    ###################################################################################################
+
+    public function getDobAttribute($value)
+    {
+        return date("d-M-Y",strtotime($value));
+    }
+
+
 }
